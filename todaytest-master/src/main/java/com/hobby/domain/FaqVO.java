a7c55505daae8cabf2c55b59083b35d21c1e931a
sujin <daftsound123@gmail.com>
2020-11-26 16:42:39 +0900

    수진 주석 및 수정

--- todaytest-master/src/main/java/com/hobby/domain/FaqVO.java
+++ todaytest-master/src/main/java/com/hobby/domain/FaqVO.java
@@ -1,15 +1,19 @@
 package com.hobby.domain;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class FaqVO {
 	//자/주/묻/는/질/문// 
-	
 	private Long faqNum;
+	//회/원/구/분//
 	private Long faqClassification;
+	//제/목//
 	private String faqTitle;
-
+	
 	//자/주/묻/는/질/문 /답//
 	private String faqContent;
+		
 }
