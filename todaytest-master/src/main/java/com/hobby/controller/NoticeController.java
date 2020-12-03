37799badbc0995d615c4f9ca4f7102d72b7d0ed8
sujin <daftsound123@gmail.com>
2020-12-03 16:16:08 +0900

    게시판 등록

--- todaytest-master/src/main/java/com/hobby/controller/NoticeController.java
+++ todaytest-master/src/main/java/com/hobby/controller/NoticeController.java
@@ -1,20 +1,61 @@
 package com.hobby.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.hobby.domain.NoticeVO;
+import com.hobby.service.NoticeService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
-import com.hobby.service.NoticeService;
+
 @Controller
 @Log4j
-@RequestMapping("/cs/*")
+@RequestMapping("/admin/*")
 @AllArgsConstructor
 public class NoticeController {
 	
 	private NoticeService service;
 	
+	@GetMapping("/register")
+	public void registerNotice() {
+		log.info("registerNoticeGetMapping.....!!!!!\n\n\n" );
+	}
 	
+	@PostMapping("/register")
+	public String registerNotice(NoticeVO notice, RedirectAttributes rttr) {
+		
+		log.info("registerPostMapping....!!!!!\\n\\n\\n" +notice);
+		
+		service.registerNotice(notice);
+		
+		rttr.addFlashAttribute("result",notice.getNtNum());
+		
+		return "redirect:/cs/faq";
+	}
+	
+	@PostMapping("/modify")
+	public String modify(@RequestParam("ntNum")Long ntNum, RedirectAttributes rttr) {
+		log.info("modify"+ntNum);
+		
+		if(service.updateNotice(ntNum)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/cs;/faq";
+	}
+//	
+//	@PostMapping("/remove")
+//	public String remove(@RequestParam("ntNum")Long ntNum, RedirectAttributes rttr) {
+//		log.info("remove"+ntNum);
+//		if (service.deleteNotice(ntNum)) {
+//			rttr.addFlashAttribute("result","success");
+//		}
+//		return "redirect:/cs/faq";
+//	}
 	
 	
 }
--- todaytest-master/src/test/java/com/hobby/mapper/NoticeMapperTests.java
+++ todaytest-master/src/test/java/com/hobby/mapper/NoticeMapperTests.java
@@ -1,48 +1,87 @@
 package com.hobby.mapper;
 
-import java.util.List;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import com.hobby.domain.NoticeCri;
-import com.hobby.domain.NoticeVO;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@WebAppConfiguration
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
 @Log4j
 public class NoticeMapperTests {
 	
-	@Setter(onMethod_ = @Autowired)
-	private FaqMapper mapper;
-	
-	@Test
-	public void testPaging() {
-		NoticeCri cri = new NoticeCri();
-		
-		cri.setPageNum(1);
-		cri.setAmount(10);
-		
-		List<NoticeVO> list = mapper.noticeListWithPaging(cri);
-		list.forEach(notice -> log.info(notice.getNtNum()));
-	}
+//	@Setter(onMethod_ = {@Autowired}) 
+//	private WebApplicationContext ctx;
+//	
+//	private MockMvc mockMvc;
+//	
+//	@Before
+//	public void setup() {
+//		this.mockMvc =MockMvcBuilders.webAppContextSetup(ctx).build();
+//	}
 	
 //	@Test
-//	public void testListPaging() throws Exception{
+//	public void testPaging() {
+//		NoticeCri cri = new NoticeCri();
 //		
-//		log.info(mockMvc.perform(
-//				MockMvcRequestBuilders.get("/cs/notice")
-//				.param("pageNum","2")
-//				.param("amount", "50"))
-//				.andReturn().getModelAndView().getModelMap());
-//
+//		cri.setPageNum(1);
+//		cri.setAmount(10);
+//		
+//		List<NoticeVO> list = mapper.noticeListWithPaging(cri);
+//		list.forEach(notice -> log.info(notice.getNtNum()));
+//	}
+	 
+//	@Test  
+//	public void testInsert() {     
+//		NoticeVO notice = new NoticeVO();  
+//		notice.setNtNum(13L); 
+//		notice.setNtUsrClassification(0L);  
+//		notice.setNtTitle("새로글");   
+//		notice.setNtContent("새로작성하는 내용");  
+//		notice.setNtDate("2012-10-09"); 
+//		
+//		mapper.insertNotice(notice);
+//		mapper.insertNotiCon(notice);
+//		
+//		log.info(notice);
+//	}
+	
+//	@Test
+//	public void testRegister() {
+//		NoticeVO notice = new NoticeVO();  
+//		notice.setNtNum(13L); 
+//		notice.setNtUsrClassification(0L);  
+//		notice.setNtTitle("새로d글");   
+//		notice.setNtContent("새로작성하d는 내용");  
+//		notice.setNtDate("2012-10-09"); 
+//		
+//		service.registerNotice(notice);
+//		
+//		log.info(notice);
+//	}
+	
+//	@Test
+//	public void testRegister() throws Exception{
+//		
+//		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/admin/register")
+//				
+//				.param("ntNum", "30L")
+//				//.param("ntUsrClassification", "30L")
+//				.param("ntTitle", "네목 ")
+//				.param("ntContent","bbbbbb!")
+//				).andReturn().getModelAndView().getViewName();
+//		log.info("###########"+resultPage);
 //	}
 }
