52668a08aae0a38879256546bbd6baefb29a84a4
dahye-jo <cool101101101@gmail.com>
2020-12-09 14:26:03 +0900

    다혜_정기모임 joinstate 보여주기 (완료) / 가입상태에 따른 가입하기,나가기 다르게 보여주기 구현 중

--- todaytest-master/src/main/java/com/hobby/service/ClubServiceImpl.java
+++ todaytest-master/src/main/java/com/hobby/service/ClubServiceImpl.java
@@ -126,4 +126,78 @@ public class ClubServiceImpl implements ClubService {
 		mapper.clubJoinMember(club, userVO);
 	}
 
+//	@Override
+//	@Transactional
+//	public boolean join(ThunderVO clubVO, UserVO loginUser, String joinState) {
+//		System.out.println("Service-join......" + clubVO);
+//		System.out.println("Service-join......" + loginUser);
+//		System.out.println("Service-join......" + joinState);		
+//		
+//		//우선적으로는 Service.join 쪽 갔을때만 -> 인원수 변동시켜주자!!!
+//			    	
+//	    //신청하기or신청취소하기 버튼을 눌렀으면 여기서 케이스를 나눠서 Mapper 메소드를 실행한다.
+//	    //joinState - 모임추방, 모임만료, 모임탈퇴, 가입승인, Null (아직 데이터 넣기 전)
+//		
+//		int result = 0;
+//		String chagneJoinState = null;
+//		
+//		if( "".equals(joinState)) {
+//	    	//null일 때 -> 가입승인 되어야 함
+//			chagneJoinState = "가입승인";
+//			clubVO.setCbCurMbnum(clubVO.getCbCurMbnum()+1);
+//			
+//			result += mapper.insertJoin(clubVO, loginUser,chagneJoinState);
+//			System.out.println("Service-join...... result : " + result);
+//			
+//			result += mapper.insertJoinHistory(clubVO, loginUser, chagneJoinState);
+//			System.out.println("Service-join...... result : " + result);
+//			
+//			
+//	    }
+//		else if(joinState.equals("모임탈퇴")) {
+//			//'모임탈퇴'일 때 -> 가입승인 되어야 함
+//			chagneJoinState = "가입승인";
+//			clubVO.setCbCurMbnum(clubVO.getCbCurMbnum()+1);
+//			
+//			result += mapper.updateJoin(clubVO, loginUser,chagneJoinState);
+//			result += mapper.insertJoinHistory(clubVO, loginUser, chagneJoinState);
+//		} 
+//		else if(joinState.equals("가입승인")) {			
+//	    	//'가입승인'일 때 -> 가입이 취소되어야함
+//			chagneJoinState = "모임탈퇴";
+//			clubVO.setCbCurMbnum(clubVO.getCbCurMbnum()-1);
+//			
+//			result += mapper.updateJoin(clubVO, loginUser,chagneJoinState);
+//			result += mapper.insertJoinHistory(clubVO, loginUser, chagneJoinState);
+//	    } 
+//	    else {
+//	    	// '모임만료'나 '모임추방'일때는  -> 가입불가 X	   
+//	    	log.info("Service-join : 아무것도 안찍힘");
+//	    }
+//	    
+//		// 멤버상태를 변경해주고 나서는, club의 현재인원을 update해준다.
+//		// 오류나서, 이거 제외하고 민성이형 주기
+//		result += mapper.update(clubVO); 	    
+//		log.info("result : " + result);
+//		System.out.println("result : " + result);
+//					
+//		// update가 call-begin-end되있어서 반환값 -1 이라 -> 2-1 =1 나옴
+//		return result == 1;
+//	}
+
+	@Override
+	public String getCbMemByUsrNum(Long usrNum, Long cbNum) {
+		log.info("getCbMemByUsrNum......" + usrNum + "/"+cbNum);
+		
+		return mapper.readCbMemByUsrNum(usrNum, cbNum);
+	}
+	
+	@Override
+	public List<ClubVO> getJoinList(Long cbNum, String cbMbStResult) {		
+		System.out.println("Service-getJoinHashMap......cbnum: " + cbNum);
+		System.out.println("Service-getJoinHashMap......cbmbstresult: " + cbMbStResult);
+		
+		return mapper.getJoinList(cbNum, cbMbStResult);
+	}
+
 }
