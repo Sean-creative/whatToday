03029e44080e5013a4e6376cad963c3dde74e712
dahye-jo <cool101101101@gmail.com>
2020-12-11 16:51:16 +0900

    소스 정리(다혜+수진+지영)

--- todaytest-master/src/test/java/com/hobby/service/ClubServiceTests.java
+++ todaytest-master/src/test/java/com/hobby/service/ClubServiceTests.java
@@ -2,15 +2,17 @@ package com.hobby.service;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
